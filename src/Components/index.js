6d7d6a47f0c5b18635f015094d9e82f256b4920c
Mwau stephen <stephenmwau41@gmail.com>
2022-10-03 22:40:26 +0800

    added components folder

---
+++ src/Components/index.js
@@ -0,0 +1,5 @@
+export { default as NavBar } from "./Layout/NavBar";
+export { default as SearchFeed } from "./Layout/SearchFeed";
+export { default as VideoDetail } from "./UI/VideoDetail";
+export { default as ChannelDetail } from "./UI/ChannelDetail";
+export { default as Feed } from "./UI/Feed";
