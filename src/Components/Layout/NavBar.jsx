3f0c51a7ababda6e6604027cf9efcc9727c5a9c4
Mwau stephen <stephenmwau41@gmail.com>
2022-10-04 19:15:16 +0800

    defined the stack properties

--- src/Components/Layout/NavBar.jsx
+++ src/Components/Layout/NavBar.jsx
@@ -4,7 +4,14 @@ import { Stack } from "@mui/material";
 import { logo } from "../../Utilities/Constants";
 
 const NavBar = () => {
-  return <Stack></Stack>;
+  return (
+    <Stack
+      direction="row"
+      alignItems="center"
+      p={2}
+      sx={{ position: "sticky", top: 0, background: "#000",  justifyContent:"space-between"}}
+    ></Stack>
+  );
 };
 
 export default NavBar;
