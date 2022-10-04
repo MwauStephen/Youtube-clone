e36216d505165189974e931d7f8c029cdf5dcb2e
Mwau stephen <stephenmwau41@gmail.com>
2022-10-04 21:42:54 +0800

    mapped through the categories array

--- src/Components/Layout/SideBar.jsx
+++ src/Components/Layout/SideBar.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { categories } from "../../Utilities/Constants";
+import { Stack } from "@mui/material";
 
 const SideBar = () => {
-  return <div>SideBar</div>;
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        overflowY: "auto",
+        height: { sx: "auto", md: "95%" },
+        flexDirection: { md: "column" },
+      }}
+    >
+      {categories.map((category) => {
+        return (
+          <button>
+            <span>{category.icon}</span>
+            <span>{category.name}</span>
+          </button>
+        );
+      })}
+    </Stack>
+  );
 };
 
 export default SideBar;
