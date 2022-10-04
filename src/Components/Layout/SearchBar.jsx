d3cd2622d3207cb0d26f77303fd393b038b535d2
Mwau stephen <stephenmwau41@gmail.com>
2022-10-04 19:55:34 +0800

    finished the search bar component styling

--- src/Components/Layout/SearchBar.jsx
+++ src/Components/Layout/SearchBar.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Paper, IconButton } from "@mui/material";
+import { Search } from "@mui/icons-material";
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  const onSubmitHandler = () => {};
+  const onChangeHandler = () => {};
+  return (
+    <Paper
+      component="form"
+      onSubmit={onSubmitHandler}
+      sx={{
+        borderRadius: 20,
+        pl: 2,
+        boxShadow: "none",
+        border: "1px solid #e3e3e3",
+        mr: { sm: 5 },
+      }}
+    >
+      <input onChange={onChangeHandler} className="search-bar" />
+      <IconButton type="submit" sx={{ color: "red", p: "10px" }}>
+        <Search />
+      </IconButton>
+    </Paper>
+  );
 };
 
 export default SearchBar;
