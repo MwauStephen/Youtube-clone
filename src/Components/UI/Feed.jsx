e26a684752fffa802b19367ac40c4b9b4b7a033d
Mwau stephen <stephenmwau41@gmail.com>
2022-10-04 21:06:17 +0800

    added the link feed section outline

--- src/Components/UI/Feed.jsx
+++ src/Components/UI/Feed.jsx
@@ -2,7 +2,26 @@ import React, { useState, useEffect } from "react";
 import { Stack, Box, Typography } from "@mui/material";
 
 const Feed = () => {
-  return <Stack></Stack>;
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "2px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        Sidebar
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright Stephen Mwau
+        </Typography>
+      </Box>
+    </Stack>
+  );
 };
 
 export default Feed;
