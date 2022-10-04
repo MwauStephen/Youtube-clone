91e5300353b9cb65d7b6597502987f963a4f810d
Mwau stephen <stephenmwau41@gmail.com>
2022-10-04 21:15:55 +0800

    added the sidebar default import

--- src/Components/index.js
+++ src/Components/index.js
@@ -4,3 +4,4 @@ export { default as VideoDetail } from "./UI/VideoDetail";
 export { default as ChannelDetail } from "./UI/ChannelDetail";
 export { default as Feed } from "./UI/Feed";
 export { default as SearchBar } from "./Layout/SearchBar";
+export { default as SideBar } from "./Layout/SideBar";
