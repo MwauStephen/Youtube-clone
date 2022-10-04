5a044ea22e3c25f85e56029ab6481e4b7b24aac3
Mwau stephen <stephenmwau41@gmail.com>
2022-10-04 19:30:03 +0800

    added default export of the searchbar

--- src/Components/index.js
+++ src/Components/index.js
@@ -3,3 +3,4 @@ export { default as SearchFeed } from "./Layout/SearchFeed";
 export { default as VideoDetail } from "./UI/VideoDetail";
 export { default as ChannelDetail } from "./UI/ChannelDetail";
 export { default as Feed } from "./UI/Feed";
+export { default as SearchBar } from "./Layout/SearchBar";
