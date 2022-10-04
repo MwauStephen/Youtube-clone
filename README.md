0b5f27dee1603d5c103f75ef111efa35a7ce7f39
Mwau stephen <stephenmwau41@gmail.com>
2022-10-04 22:03:07 +0800

    finished creating the sidebar component

--- README.md
+++ README.md
@@ -68,3 +68,9 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+<!-- Material Ui components -->
+
+## `Stack`- is just a simple div element
+
+## `Paper`-an simple element with a white background that resembles a paper
--- src/Components/Layout/SideBar.jsx
+++ src/Components/Layout/SideBar.jsx
@@ -3,6 +3,7 @@ import { categories } from "../../Utilities/Constants";
 import { Stack } from "@mui/material";
 
 const SideBar = () => {
+  const selectedCategory = "New";
   return (
     <Stack
       direction="row"
@@ -14,9 +15,29 @@ const SideBar = () => {
     >
       {categories.map((category) => {
         return (
-          <button>
-            <span>{category.icon}</span>
-            <span>{category.name}</span>
+          <button
+            className="category-btn"
+            style={{
+              background: category.name === selectedCategory && "#fc1503",
+              color: "#fff",
+            }}
+            key={category.name}
+          >
+            <span
+              style={{
+                color: category.name === selectedCategory ? "white" : "red",
+                marginRight: "15px",
+              }}
+            >
+              {category.icon}
+            </span>
+            <span
+              style={{
+                opacity: category.name === selectedCategory ? "1" : 0.8,
+              }}
+            >
+              {category.name}
+            </span>
           </button>
         );
       })}
