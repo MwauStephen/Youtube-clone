06da7a5bef8cf2de230278d72719a238ccb5f8a0
Mwau stephen <stephenmwau41@gmail.com>
2022-10-07 10:03:47 +0800

    added the options api object

--- src/Utilities/fetchFromApi.jsx
+++ src/Utilities/fetchFromApi.jsx
@@ -1,16 +1,14 @@
 import axios from "axios";
 
+const BASE_URL = "https://youtube-v31.p.rapidapi.com/search";
 const options = {
   method: "GET",
-  url: "https://youtube-v31.p.rapidapi.com/search",
+  url: BASE_URL,
   params: {
-    relatedToVideoId: "7ghhRHRP6t4",
-    part: "id,snippet",
-    type: "video",
     maxResults: "50",
   },
   headers: {
-    "X-RapidAPI-Key": "63a99cba99mshf78883e03c717b7p1808aejsnc2918390d6d5",
+    "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
 };
