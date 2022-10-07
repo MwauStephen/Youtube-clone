a21959d7f29e0040bf39e942f8e86cf69a442e54
Mwau stephen <stephenmwau41@gmail.com>
2022-10-07 09:43:55 +0800

    added an import

--- src/Components/index.js
+++ src/Components/index.js
@@ -5,3 +5,5 @@ export { default as ChannelDetail } from "./UI/ChannelDetail";
 export { default as Feed } from "./UI/Feed";
 export { default as SearchBar } from "./Layout/SearchBar";
 export { default as SideBar } from "./Layout/SideBar";
+
+export { default as Vidoes } from "./Videos/Videos";
