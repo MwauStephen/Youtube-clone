ff4061fb938b4894f63cb3e7f5105a1a3128923a
MwauStephen <stephenmwau41@gmail.com>
2022-10-11 20:32:50 +0300

    imported the default demo data

--- src/Components/Videos/VideoCard.jsx
+++ src/Components/Videos/VideoCard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import {Typography,Card,CardContent,CardMedia} from "@mui/material";
 import { CheckCircle } from '@mui/icons-material';
+import {defaultThumbnailUrl,demoVideoUrl,demoVideoTitle,demoChannelUrl,demoChannelTitle} from "../../Utilities/Constants";
 
 const VideoCard = () => {
   return (
