62f36ddc7a16150b5523df29e3df4725eda1f8e9
MwauStephen <stephenmwau41@gmail.com>
2022-10-11 20:19:52 +0300

    mapped through the video results

--- src/Components/Videos/Videos.jsx
+++ src/Components/Videos/Videos.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import VideoCard from "./VideoCard";
+import ChannelCard from "./ChannelCard";
 import {Stack,Box} from "@mui/material"
 
 const Videos = (props) => {
-return <Stack direction="row" flexWrap="wrap" gap={2} sx={{backgroundColor:"orangered"}} justifyContent="start">
-    <Box>Vidoe section</Box>
-</Stack>
+    return <Stack direction="row" flexWrap="wrap" gap={2} justifyContent="start" sx={{backgroundColor:"orangered"}}>
+        {props.videos.map((item,id)=>{
+          return <Box key={id}>
+                {item.id.videoId && <VideoCard video={item}/>}
+                {item.id.channelId && <ChannelCard channelDetail={item}/>}
+            </Box>
+        })}
+    </Stack>
 };
 
 export default Videos;
