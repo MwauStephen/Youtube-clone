54716babd7fcdc8e37952d9cfb87061ba4ff394e
MwauStephen <stephenmwau41@gmail.com>
2022-10-11 19:37:27 +0300

    imported the stack and box component

--- src/Components/Videos/Videos.jsx
+++ src/Components/Videos/Videos.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import {Stack,Box} from "@mui/material"
 
 const Videos = (props) => {
- console.log(props.videos)
+return <Stack direction="row" flexWrap="wrap" gap={2} sx={{backgroundColor:"orangered"}} justifyContent="start">
+    <Box>Vidoe section</Box>
+</Stack>
 };
 
 export default Videos;
