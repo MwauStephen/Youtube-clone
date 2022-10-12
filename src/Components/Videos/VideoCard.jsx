ed82700691d91771151bb5193cf7d06925f49133
MwauStephen <stephenmwau41@gmail.com>
2022-10-12 06:04:17 +0300

    render the video contents

--- src/Components/Videos/VideoCard.jsx
+++ src/Components/Videos/VideoCard.jsx
@@ -4,9 +4,17 @@ import {Typography,Card,CardContent,CardMedia} from "@mui/material";
 import { CheckCircle } from '@mui/icons-material';
 import {defaultThumbnailUrl,demoVideoUrl,demoVideoTitle,demoChannelUrl,demoChannelTitle} from "../../Utilities/Constants";
 
-const VideoCard = () => {
+const VideoCard = ({video:{id:{videoId},snippet}})=>{
+
+console.log(snippet)
+
+
+  
   return (
-    <div>VideoCard</div>
+    <Card>
+      <Link to={videoId ? `/video/${videoId}`:demoVideoUrl}></Link>
+     <CardMedia  image={snippet.thumbnails.high.url} alt={snippet.title} sx={{width:358, height:180}}></CardMedia>
+    </Card>
   )
 }
 
