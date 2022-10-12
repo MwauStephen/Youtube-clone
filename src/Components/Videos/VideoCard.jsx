4b4792299c1c1986962c0dce0b785f4d52be0b45
MwauStephen <stephenmwau41@gmail.com>
2022-10-12 06:31:34 +0300

    added the video card contents

--- src/Components/Videos/VideoCard.jsx
+++ src/Components/Videos/VideoCard.jsx
@@ -12,8 +12,25 @@ console.log(snippet)
   
   return (
     <Card>
-      <Link to={videoId ? `/video/${videoId}`:demoVideoUrl}></Link>
+
+      <Link to={videoId ? `/video/${videoId}`:demoVideoUrl}>
      <CardMedia  image={snippet.thumbnails.high.url} alt={snippet.title} sx={{width:358, height:180}}></CardMedia>
+     </Link>
+     
+     <CardContent sx={{backgroundColor:"#1e1e1e", height:"106px"}}>
+
+
+      <Link to={videoId ? `/video/${videoId}`:demoVideoTitle}>
+      <Typography variant="subtitle1" fontWeight="bold" color="#fff">{snippet.title.slice(0,60) || demoVideoTitle.slice(0,60)}</Typography>
+      </Link>
+
+
+      <Link to={snippet.channelId ? `/channel/${snippet.channelId}`:demoChannelUrl}>
+      <Typography variant="subtitle2" fontWeight="bold" color="gray">{snippet.channelTitle || demoChannelTitle}</Typography>
+      </Link>
+
+
+     </CardContent>
     </Card>
   )
 }
