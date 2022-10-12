06fb38c799d42983cd5dbbb68d6bdf9ce8afd7f3
MwauStephen <stephenmwau41@gmail.com>
2022-10-12 06:04:46 +0300

    commented out the channel card component

--- src/Components/Videos/Videos.jsx
+++ src/Components/Videos/Videos.jsx
@@ -4,11 +4,12 @@ import ChannelCard from "./ChannelCard";
 import {Stack,Box} from "@mui/material"
 
 const Videos = (props) => {
-    return <Stack direction="row" flexWrap="wrap" gap={2} justifyContent="start" sx={{backgroundColor:"orangered"}}>
+//    console.log(props.videos)
+    return <Stack direction="row" flexWrap="wrap" gap={2} justifyContent="start">
         {props.videos.map((item,id)=>{
-          return <Box key={id} sx={{backgroundColor:"green", color:"white"}}>
+          return <Box key={id}>
                 {item.id.videoId && <VideoCard video={item}/>}
-                {item.id.channelId && <ChannelCard channelDetail={item}/>}
+                {/* {item.id.channelId && <ChannelCard channelDetail={item}/>} */}
             </Box>
         })}
     </Stack>
