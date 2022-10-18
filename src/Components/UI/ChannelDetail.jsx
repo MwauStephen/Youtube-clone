33fdf926bffd7aef5d3939727864b49abdf09ba4
mwaustephen <stephenmwau41@gmail.com>
2022-10-18 22:44:04 +0300

    added the basic material ui components

--- src/Components/UI/ChannelDetail.jsx
+++ src/Components/UI/ChannelDetail.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import {Box,Typogrpahy,CardContent,CardMedia} from "@mui/material";
+import { CheckCircle } from '@mui/icons-material';
+import { demoProfilePicture } from '../../Utilities/Constants';
+import {Link} from "react-router-dom";
 
-const ChannelDetail = () => {
+const ChannelDetail = (props) => {
   return (
     <div>ChannelDetail</div>
   )
