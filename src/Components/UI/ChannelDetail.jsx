1377fc02887397246b4f85ba9b6c2660d0062c91
mwaustephen <stephenmwau41@gmail.com>
2022-10-18 23:26:52 +0300

    removed the wrongly imported components

--- src/Components/UI/ChannelDetail.jsx
+++ src/Components/UI/ChannelDetail.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
-import {Box,Typogrpahy,CardContent,CardMedia} from "@mui/material";
-import { CheckCircle } from '@mui/icons-material';
-import { demoProfilePicture } from '../../Utilities/Constants';
-import {Link} from "react-router-dom";
+
 
 const ChannelDetail = (props) => {
   return (
