3ed2977c0a356ae1053f273a6be617c5b8f7c7d5
mwaustephen <stephenmwau41@gmail.com>
2022-10-21 20:48:31 +0300

    imported the fetch component

--- src/Components/UI/ChannelDetail.jsx
+++ src/Components/UI/ChannelDetail.jsx
@@ -4,8 +4,10 @@ import { Box } from "@mui/material";
 import { useParams } from "react-router-dom";
 import { Videos } from "../Videos/Videos";
 import { channelCard } from "../Videos/ChannelCard";
+import fetchFromApi from "../../Utilities/fetchFromApi";
 
 const ChannelDetail = (props) => {
+  const [channelDetail, setChannelDetail] = useState(null);
   const { id } = useParams();
 
   return <div>{id}</div>;
