4367aa3ff4e256b230319a5edbc034ac228acde5
mwaustephen <stephenmwau41@gmail.com>
2022-10-21 20:46:54 +0300

    changed the videos import

--- src/Components/index.js
+++ src/Components/index.js
@@ -6,6 +6,6 @@ export { default as Feed } from "./UI/Feed";
 export { default as SearchBar } from "./Layout/SearchBar";
 export { default as SideBar } from "./Layout/SideBar";
 
-export { default as Vidoes } from "./Videos/Videos";
+export { default as Videos } from "./Videos/Videos";
 export {default as VideoCard} from "./Videos/VideoCard";
 export {default  as ChannelCard} from "./Videos/ChannelCard";
