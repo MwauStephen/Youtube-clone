2e5c10427c186dbd79d9fde7ab3b7ed879a486f3
mwaustephen <stephenmwau41@gmail.com>
2022-10-21 20:46:26 +0300

    imported some components to be used in this component

--- src/Components/UI/ChannelDetail.jsx
+++ src/Components/UI/ChannelDetail.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-
+import React from "react";
+import { useState, useEffect } from "react";
+import { Box } from "@mui/material";
+import { useParams } from "react-router-dom";
+import { Videos } from "../Videos/Videos";
+import { channelCard } from "../Videos/ChannelCard";
 
 const ChannelDetail = (props) => {
-  return (
-    <div>ChannelDetail</div>
-  )
-}
+  const { id } = useParams();
+
+  return <div>{id}</div>;
+};
 
 export default ChannelDetail;
