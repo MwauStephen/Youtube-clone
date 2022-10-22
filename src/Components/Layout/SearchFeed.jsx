8da8c10a31e2a6e78f2534e33676faade53e1e8e
mwaustephen <stephenmwau41@gmail.com>
2022-10-22 14:22:44 +0300

    imported the  use param hook from react router dom

--- src/Components/Layout/SearchFeed.jsx
+++ src/Components/Layout/SearchFeed.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Box, Typography } from "@mui/material";
+import { useParams } from "react-router-dom";
+
+import Videos from "../Videos/Videos";
+import fetchFromApi from "../../Utilities/fetchFromApi";
 
 const SearchFeed = () => {
-  return <div>SearchFeed</div>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromApi(`search?part=snippet&q`).then((data) => {
+      setVideos(data.items);
+    });
+  }, []);
+  // console.log(videos);
+
+  return (
+    <Box
+      sx={{
+        flex: 2,
+        height: "90vh",
+        overflowY: "auto",
+      }}
+      p={2}
+    >
+      <Typography variant="h4" fontWeight="bold" sx={{ color: "white" }} mb={2}>
+        Search results for:
+        <span style={{ color: "#fc1503" }}>Videos</span>
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
 };
 
 export default SearchFeed;
