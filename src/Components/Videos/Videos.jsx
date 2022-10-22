c00df40cbaa1dde67ccba056398e813bbc8c0efe
mwaustephen <stephenmwau41@gmail.com>
2022-10-22 13:25:14 +0300

    checked for props  error

--- src/Components/Videos/Videos.jsx
+++ src/Components/Videos/Videos.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import VideoCard from "./VideoCard";
 import ChannelCard from "./ChannelCard";
-import {Stack,Box} from "@mui/material"
+import { Stack, Box } from "@mui/material";
 
 const Videos = (props) => {
-//    console.log(props.videos);
-    return <Stack direction="row" flexWrap="wrap" gap={2} justifyContent="spaceBetween">
-        {props.videos.map((item,id)=>{
-          return <Box key={id}>
-                {item.id.videoId && <VideoCard video={item}/>}
-                {item.id.channelId && <ChannelCard channelDetail={item}/>}
-            </Box>
-        })}
+  // console.log(props.videos);
+  return (
+    <Stack
+      direction="row"
+      flexWrap="wrap"
+      gap={2}
+      justifyContent="spaceBetween"
+    >
+      {props.videos.map((item, id) => {
+        // console.log(item);
+        return (
+          <Box key={id}>
+            {item.id.videoId && <VideoCard video={item} />}
+
+            {item.id.channelId && <ChannelCard channelDetail={item} />}
+          </Box>
+        );
+      })}
     </Stack>
+  );
 };
 
 export default Videos;
